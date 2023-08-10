0b9f47435e767769e2b36270673ebbc9f522139b
Lingkon <becomelingkon@gmail.com>
2023-08-10 16:40:21 +0600

    013-type-conversion

--- 006-variable.js
+++ 006-variable.js
@@ -5,3 +5,6 @@ var myCourse = "Javascript course"; // variable camel case
 console.log("my name is " + myName);
 console.log("my age is " + myAge); // is er por use space
 console.log("I start " + myCourse + " when my age was " + myAge); //start er por space,,, when er-age-space , was er por spae
+
+// Extra way to define variable
+let shopping_cart_items = ["ATM", "makeup", "sunglass"];
