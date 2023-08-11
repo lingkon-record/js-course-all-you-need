930b0af196acc56641a48155660d778eb649739a
Lingkon <becomelingkon@gmail.com>
2023-08-11 17:05:43 +0600

    // Comparison Operators

--- try-objects-run.js
+++ try-objects-run.js
@@ -1,1 +1,12 @@
+const person = {
+  name: "John",
+  age: 30,
+  address: {
+    street: "123 Main road St",
+    city: "New York",
+  },
+};
 
+console.log("My friend, " + person.name);
+console.log(person.address.street);
+console.log("He is from: " + person.address.city);
