ea27245ab3564de2af817fa8f43c1e401022f8cf
Lingkon <becomelingkon@gmail.com>
2023-08-11 10:01:53 +0600

    014-octal hexa

---
+++ 014-octal-hexa.js
@@ -0,0 +1,12 @@
+// Octal numbers use "8"
+// hexadecimal numbers use "16"
+
+// rule oct = 0...
+var octalNum = 0756; // it works
+console.log(octalNum);
+
+//rule hex = 0x...
+var hexaNum = 0xff;
+console.log(hexaNum);
+
+/*  https://courses.cs.vt.edu/~csonline/NumberSystems/Lessons/HexAndOctalNumbers/index.html  */
