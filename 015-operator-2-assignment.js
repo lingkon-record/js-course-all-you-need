e50a56eb2e5657b362a05e7c323aec1dc0f6d553
Lingkon <becomelingkon@gmail.com>
2023-08-11 11:22:27 +0600

    Assignment Operator adds

--- 015-operator-2-assignment.js
+++ 015-operator-2-assignment.js
@@ -1,1 +1,23 @@
-hi;
+let rakib = (rony = rasel = simu = "Play Cricket ||");
+console.log(
+  "Rakib daily " + rakib,
+  "Rony always like to " + rony,
+  "Rasel Everyday " + rasel,
+  "Simu sad bcz I " + simu
+);
+
+//Addition Assignment Operator adds a value to a variable.
+var wallet = 2000;
+var nft_sell = 2000;
+wallet += crypto;
+
+console.log(nft_sell); // 2K
+console.log(wallet); // 4K
+
+// Subtraction Assignment Operator subtracts a value from a variable.
+var mainWallet = 1500;
+var bitcoinInvest = 300;
+mainWallet -= bitcoinInvest;
+
+console.log(bitcoinInvest); // 300
+console.log(mainWallet); // 1200
