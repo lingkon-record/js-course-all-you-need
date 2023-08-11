cc21e032ddef09c96730e2e0ee2a7795e4cdc4ee
Lingkon <becomelingkon@gmail.com>
2023-08-11 10:45:30 +0600

    operator-1 update

--- 015-operator-3-mix.js
+++ 015-operator-3-mix.js
@@ -1,3 +1,6 @@
 // Operator precedence : describes the order in which operations are performed in an arithmetic expression.
 let count_serial = 40 + 20 * 3;
-console.log(count_serial);
+console.log(count_serial); // 40 + (20*3)
+
+let x = (100 / 50) * 3;
+console.log(x);
