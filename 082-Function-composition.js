38dcfb321dc964e8cff9bef239fca94126cbb826
MD. LINGKON ISLAM <111384782+lingkon-record@users.noreply.github.com>
2024-01-25 00:20:01 +0600

    Update 082-Function-composition.js

--- 082-Function-composition.js
+++ 082-Function-composition.js
@@ -19,7 +19,18 @@ const result = composedFunction(3);
 console.log(result); // Output: 121
 
 
-
 /*
-Function composition is the process of combining two or more functions to produce a new function. 
+Function composition is the process ofcombining 
+two or more functions to produce a new function. 
 */
+
+// Functions to compose
+const double = (x) => x * 2;
+const increment = (x) => x + 1;
+
+// Function composition
+const composedFunction2 = (x) => increment(double(x));
+
+// Usage
+const result2 = composedFunction2(5);
+console.log(result2); // Output: 11
